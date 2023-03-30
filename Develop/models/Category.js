7f084d8acbd242ba0dd7b8b90832c06d76a14e32
aya effoua <carineeffoua@gmail.com>
2023-03-30 13:53:53 -0400

    deleted folder

---
+++ Develop/models/Category.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Category extends Model {}
+
+Category.init(
+  {
+    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncremment:true
+
+    },
+    category_name: {
+     type: DataTypes.STRING,
+     allowNull: false,
+    }
+  },
+  // add next columns on each file
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'category',
+  }
+);
+
+module.exports = Category;
